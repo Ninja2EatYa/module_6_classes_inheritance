00e502a0b39b79f1ab0fac0ca2b8a69b308c4660
Ninja2EatYa <alexandrov.peter@gmail.com>
2024-07-23 14:10:45 +0300

    Они все так похожи.py

--- Они все так похожи.py
+++ Они все так похожи.py
@@ -1,10 +1,17 @@
 import math
 
+
 class Figure:
     sides_count = 0
 
-    def __init__(self, __color, *__sides):
-        self.__sides = list(__sides) if self.__is_valid_sides(*__sides) else [1] * self.sides_count
+    def __init__(self, __color, __sides):
+        if self.__is_valid_sides(*__sides):
+            if isinstance(self, Cube):
+                self.__sides = list(__sides) * self.sides_count
+            else:
+                self.__sides = list(__sides)
+        else:
+            self.__sides = [1] * self.sides_count
         self.__color = list(__color) if self.__is_valid_color(*__color) else [0, 0, 0]
         self.filled = False
 
@@ -19,7 +26,12 @@ class Figure:
             self.__color = [r, g, b]
 
     def __is_valid_sides(self, *__sides):
-        return all(isinstance(side, int) and side > 0 for side in __sides) and len(__sides) == self.sides_count
+        cond1 = all(isinstance(side, int) and side > 0 for side in __sides)
+        if isinstance(self, Cube):
+            cond2 = len(__sides) == 1
+        else:
+            cond2 = len(__sides) == self.sides_count  #  или сравнить с len(self.__sides) вместо self.sides_count
+        return cond1 and cond2
 
     def get_sides(self):
         return self.__sides
@@ -29,18 +41,24 @@ class Figure:
 
     def set_sides(self, *new_sides):
         if self.__is_valid_sides(*new_sides):
-            self.__sides = list(new_sides)
+            if isinstance(self, Cube):
+                self.__sides = list(new_sides) * self.sides_count
+            else:
+                self.__sides = list(new_sides)
 
 
 class Circle(Figure):
     sides_count = 1
 
     def __init__(self, __color, *__sides):
-        super().__init__(__color, *__sides)
+        super().__init__(__color, __sides)
         self.__radius = self.__calculate__radius()
 
     def __calculate__radius(self):
         return self.get_sides()[0] / (2 * math.pi)
+
+    def get_radius(self):
+        return self.__radius
 
     def get_square(self):
         return math.pi * (self.__radius ** 2)
@@ -50,7 +68,7 @@ class Triangle(Figure):
     sides_count = 3
 
     def __init__(self, __color, *__sides):
-        super().__init__(__color, *__sides)
+        super().__init__(__color, __sides)
         self.__height = self.__calculate__height()
 
     def __calculate__height(self):
@@ -66,22 +84,13 @@ class Cube(Figure):
     sides_count = 12
 
     def __init__(self, __color, *__sides):
-        super().__init__(__color, *__sides)
-        if len(__sides) != 1:
-            self._Figure__sides = [1] * self.sides_count
-        else:
-            self._Figure__sides = [__sides[0]] * self.sides_count
-
-    def set_sides(self, *new_sides):
-        if len(new_sides) != 1:
-            self.__sides = [super().get_sides()[0]] * self.sides_count
-        else:
-            self.__sides = new_sides
+        super().__init__(__color, __sides)
 
     def get_volume(self):
-        return self.__sides[0] ** 2 * 6
+        return super().get_sides()[0] ** 2 * 6
 
 
+print('Из задания:')
 circle1 = Circle((200, 200, 100), 10)  # (Цвет, стороны)
 cube1 = Cube((222, 35, 130), 6)
 
@@ -102,3 +111,58 @@ print(len(circle1))
 
 # Проверка объёма (куба):
 print(cube1.get_volume())
+
+
+# круг
+print('\n\n\nКруг:')
+circle1 = Circle((200, 200, 100), 10)
+print('стороны:', circle1.get_sides())
+print('прощадь:', circle1.get_square())
+print('радиус:', circle1.get_radius())
+print('цвета:', circle1.get_color())
+circle1.set_color(1, 2, 3)
+print('цвета:', circle1.get_color())
+circle1.set_color(-1, 200, 3)
+print('цвета:', circle1.get_color())
+circle1.set_color(1, 2, 300.0)
+print('цвета:', circle1.get_color())
+circle1.set_sides(15)
+print('стороны:', circle1.get_sides())
+print('прощадь:', circle1.get_square())  # не меняет площадь
+print('радиус:', circle1.get_radius())  # не меняет радиус
+circle1.set_sides(10, 8)
+print('стороны:', circle1.get_sides())
+
+# треугольник
+print('\n\n\nТреугольник:')
+fig = Triangle((200, 200, 100), 10)
+print('стороны:', fig.get_sides())
+print('прощадь:', fig.get_square())  # + получение высоты треугольника
+print('цвета:', fig.get_color())
+
+fig = Triangle((200, 200, 100), 10, 7, 9)
+print('стороны:', fig.get_sides())
+fig.set_sides(1)
+print('стороны:', fig.get_sides())
+fig.set_sides(1, 2, 3)
+print('стороны:', fig.get_sides())
+fig.set_sides(1, 2, -3)
+print('стороны:', fig.get_sides())
+
+# куб
+print('\n\n\nКуб:')
+fig = Cube((200, 200, 100), 10, 12)
+print('стороны:', fig.get_sides())
+print('прощадь:', fig.get_volume())
+print('цвета:', fig.get_color())
+
+fig = Cube((200, 200, 100), 10)
+print('стороны:', fig.get_sides())
+fig.set_sides(50)
+print('стороны:', fig.get_sides())
+fig.set_sides(-100)
+print('стороны:', fig.get_sides())
+fig.set_sides(1, 2, 3)
+print('стороны:', fig.get_sides())
+fig.set_sides(1, 2, -3)
+print('стороны:', fig.get_sides())
